2758f238d4513de2ee8bcc341085e24f0a0b34ae
linuxN <147541775@qq.com>
2018-03-05 18:28:44 +0800

    updata

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,16 @@
 package routers
 
 import (
-	"loveHome/controllers"
 	"github.com/astaxie/beego"
+	"loveHome/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/api/v1.0/areas", &controllers.AreaController{}, "get:GetAreaInfo")
+	beego.Router("/api/v1.0/session", &controllers.SessionController{}, "get:GetSessionName;delete:DelSessionName")
+	beego.Router("/api/v1.0/houses/index", &controllers.HousesIndexController{}, "get:GetHousesIndex")
+
+	beego.Router("/api/v1.0/users", &controllers.UserController{}, "post:Rec")
+	beego.Router("/api/v1.0/sessions", &controllers.UserController{}, "post:Login")
 }
